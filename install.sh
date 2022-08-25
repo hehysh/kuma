24b89c3b1b87932f45430fbbacbc8347304739ea
hehysh <89674984+hehysh@users.noreply.github.com>
2022-08-25 14:37:02 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Update your npm to the latest version
+npm install npm -g
+git clone https://github.com/louislam/uptime-kuma.git
+cd uptime-kuma
+npm run setup
+# Install PM2 if you don't have: npm install pm2 -g
+npm install pm2 -g
+pm2 start server/server.js --name uptime-kuma
+# Listen to different port or hostname
+pm2 start server/server.js --name uptime-kuma -- --port=80 --host=0.0.0.0
